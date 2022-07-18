af22c6d6af1cfa6bef8d7ac772eb8a503a725cbf
Ernests Cers <ernests.cers@atea.com>
2022-07-18 15:05:01 +0300

    get repos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+class Repo extends React.PureComponent {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      repoName: [],
+      userName: ''
+    };
+  }
+
+  componentDidMount() {
+    let repoNames = [];
+    axios.get(`https://api.github.com/users/${this.state.userName}/repos`)
+      .then(({ data }) => {
+        for (let index = 0; index < data.length; index++) {
+          repoNames[index] = data[index].name;
+        }
+        this.setState({
+          repoName: repoNames
+        });
+      })
+      .catch(error => console.log(error.message));
+    // console.log(this.state);
+
+  }
+
+  orderNames(repoNames) {
+    let ordered = [];
+    for (let index = 0; index < repoNames.length; index++) {
+      ordered[index] = <li>{repoNames[index]}</li>
+    }
+    return ordered;
+  }
+
+  handleChange(event){
+    this.setState({userName: event.target.value});
+    console.log(this.state.userName);
+    
+  }
+
+  render() {
+    const names = this.orderNames(this.state.repoName);
+    this.componentDidMount();
+    return (
+      <div>
+        <form>
+        <label>
+          UserName
+          <input type="text" value={this.state.userName} onChange={(event) => {this.handleChange(event);}} />
+        </label>
+      </form>
+        <ol>
+          {names}
+        </ol>
+      </div>
+    );
+  }
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Repo />);
